a068cdd8aa88ab71ebba6ee1bcbfb65dfba11bd0
Guilherme <gui_g88@hotmail.com>
2022-03-22 15:08:26 -0300

    fourth commit

--- client/src/components/CreateExercise.tsx
+++ client/src/components/CreateExercise.tsx
@@ -1,20 +1,99 @@
-import { useState } from 'react';
+import { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const initialState = {
   username: '',
   description: '',
   duration: 0,
   date: new Date(),
-  users: [],
+  users: [''],
 };
 
 export const CreateExercise = () => {
   const [exercise, setExercise] = useState(initialState);
+  const navigate = useNavigate();
 
-  const onChangeHandler = (e: any) => {
-    const { name, value } = e.target;
+  const onChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+    if (name === 'duration') {
+      setExercise((props) => ({ ...props, [name]: Number(value) }));
+      return;
+    }
     setExercise((props) => ({ ...props, [name]: value }));
   };
 
-  return <div>CreateExercise</div>;
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { username, description, duration, date } = exercise;
+    console.log({ username, description, duration, date });
+    navigate('/');
+  };
+
+  useEffect(() => {
+    setExercise((initialState) => ({
+      ...initialState,
+      users: ['test user'],
+      username: 'test user',
+    }));
+  }, []);
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <label>Username: </label>
+        <select
+          required
+          name='username'
+          value={exercise.username}
+          onChange={onChange}
+        >
+          {exercise.users.map((user) => (
+            <option key={user} value={user}>
+              {user}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div>
+        <label>Description: </label>
+        <input
+          type='text'
+          required
+          name='description'
+          value={exercise.description}
+          onChange={onChange}
+        />
+      </div>
+
+      <div>
+        <label>Duration (in minutes): </label>
+        <input
+          type='text'
+          required
+          name='duration'
+          value={exercise.duration}
+          onChange={onChange}
+        />
+      </div>
+
+      <div>
+        <label>Date: </label>
+        <DatePicker
+          selected={exercise.date}
+          onChange={(date: Date) =>
+            setExercise((props) => ({ ...props, date: date }))
+          }
+        />
+      </div>
+
+      <button style={{ fontFamily: 'inherit', cursor: 'pointer' }}>
+        Submit
+      </button>
+    </form>
+  );
 };
